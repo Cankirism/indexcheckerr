8eba6da1efcabcf108b79486ea0468e58a2bf62c
cankirism <cankiri.bilsis@saglik.gov.tr>
2024-09-17 14:58:22 +0300

    result table changched

--- client/src/Components/Table.jsx
+++ client/src/Components/Table.jsx
@@ -18,89 +18,91 @@ const Table = ({data})=>{
    },[data])
     return(
         <>
-<div class=" bg-gray-50">
-  <div class="mx-auto max-w-screen-xl px-2 py-10">
-    
-    <div class="mt-6 overflow-hidden rounded-xl bg-white px-6 shadow lg:px-4">
-      <table class={visible} >
-        <thead class=" hidden text-left  border-b lg:table-header-group">
-          <tr class="">
-           
+<div class="mx-auto max-w-screen-lg px-4 py-8 sm:px-8">
+  <div class="flex items-center justify-between pb-6">
+    {/* <div>
+      <h2 class="font-semibold text-gray-700">User Accounts</h2>
+      <span class="text-xs text-gray-500">View accounts of registered users</span>
+    </div> */}
+    {/* <div class="flex items-center justify-between">
+      <div class="ml-10 space-x-8 lg:ml-40">
+        <button class="flex items-center gap-2 rounded-md bg-blue-600 px-4 py-2 text-sm font-semibold text-white focus:outline-none focus:ring hover:bg-blue-700">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75" />
+          </svg>
 
-            <td class="whitespace-normal py-4 text-sm font-bold text-gray-500 sm:px-3">#</td>
-            <td class="whitespace-normal py-4 text-sm font-bold text-gray-500 sm:px-3">Url</td>
-            <td class="whitespace-normal py-4 text-sm font-bold text-gray-500 sm:px-3">Google</td>
-            <td class="whitespace-normal py-4 text-sm font-bold text-gray-500 sm:px-3">Bing</td>
+          CSV
+        </button>
+      </div>
+    </div> */}
+  </div>
+  <div class="overflow-y-hidden rounded-lg border">
+    <div class="overflow-x-auto">
+      <table class="w-full">
+        <thead>
+          <tr class="bg-blue-600 text-left text-xs font-semibold uppercase tracking-widest text-white">
+            <th class="px-5 py-3">#</th>
+            <th class="px-5 py-3">Url</th>
+            <th class="px-5 py-3">Google</th>
+            <th class="px-5 py-3">Bing</th>
+            
           </tr>
         </thead>
+        <tbody class="text-gray-500">
+          {dat.map((dt,index,key)=>
+          (
+            <tr>
+              <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+              <p class="whitespace-no-wrap">{index+1}</p>
+              </td>
+              <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                {dt.url}
+              </td>
+              {
+              dt.result.google?(
+                <td class="border-b border-gray-200 bg-white px-5 py-5 text-green-600 text-sm">
+                  indexed
+                </td>
 
-        <tbody class="bg-white lg:border-gray-300 text-center">
-     {
-         dat.map((dt,index,key)=>(
-            <tr class="">
-           
+              ):(
+                <td class="border-b border-gray-200 bg-white text-red-500 px-5 py-5 text-sm">
+                Not indexed
+              </td>
 
+              )
+              
+              }
+              {
+              dt.result.bind?(
+                <td class="border-b border-gray-200 bg-white text-green-600 px-5 py-5 text-sm">
+                indexed
+              </td>
+              ):(
+                <td class="border-b border-gray-200 bg-white  text-red-500 px-5 py-5 text-sm">
+                Not indexed
+              </td>
+              )
+              }
+
+            </tr>
+
+          )
             
-            <td class="whitespace-no-wrap hidden py-4 text-center text-sm text-gray-600 sm:px-3 lg:table-cell lg:text-left">{index+1}</td>
-            <td class="whitespace-no-wrap hidden py-4 text-center text-sm text-gray-600 sm:px-3 lg:table-cell lg:text-left">{dt.url}</td>
-            {dt.result.google?(
-                <td class="whitespace-no-wrap hidden py-4 text-center text-sm text-green-600 sm:px-3 lg:table-cell lg:text-left">indexed</td>
-
-            ):(
-                <td class="whitespace-no-wrap hidden py-4 text-center text-sm text-red-600 sm:px-3 lg:table-cell lg:text-left">Not Indexed</td>
-            )}
-
-            {dt.result.bind?(
-                 <td class="whitespace-no-wrap hidden py-4 text-center text-sm text-green-600 sm:px-3 lg:table-cell lg:text-left">indexed</td>
-            ):(
-                <td class="whitespace-no-wrap hidden py-4 text-center text-sm text-red-600 sm:px-3 lg:table-cell lg:text-left">Not indexed</td>
-            )
-
-            }
-            
-           
-          </tr>
-
-         
-       
-    ))
-     }
-     
-       {/* {
-        dat.count>0?
-    
-            dat.map((dt,index)=>(
-                <tr class="">
-               
-    
-                
-                <td class="whitespace-no-wrap hidden py-4 text-center text-sm text-gray-600 sm:px-3 lg:table-cell lg:text-left">{index}</td>
-                <td class="whitespace-no-wrap hidden py-4 text-center text-sm text-gray-600 sm:px-3 lg:table-cell lg:text-left">{dat.url}</td>
-                <td class="whitespace-no-wrap hidden py-4 text-center text-sm text-gray-600 sm:px-3 lg:table-cell lg:text-left">{dat.result.google}</td>
-                <td class="whitespace-no-wrap hidden py-4 text-center text-sm text-gray-600 sm:px-3 lg:table-cell lg:text-left">{dat.result.bing}</td>
-              </tr>
-    
-             
-           
-        )):(
-            <tr class="">
-               
-    
-                
-            <td class="whitespace-no-wrap hidden py-4 text-center text-sm text-gray-600 sm:px-3 lg:table-cell lg:text-left">"asd"</td>
-            <td class="whitespace-no-wrap hidden py-4 text-center text-sm text-gray-600 sm:px-3 lg:table-cell lg:text-left">"aSD"</td>
-            <td class="whitespace-no-wrap hidden py-4 text-center text-sm text-gray-600 sm:px-3 lg:table-cell lg:text-left">"ASDASD"</td>
-            <td class="whitespace-no-wrap hidden py-4 text-center text-sm text-gray-600 sm:px-3 lg:table-cell lg:text-left">"ASDASD"</td>
-          </tr>
-        )
-    } */}
-         
-         
+          )}
+          
         </tbody>
       </table>
     </div>
+    <div class="flex flex-col items-center border-t bg-white px-5 py-5 sm:flex-row sm:justify-between">
+      <span class="text-xs text-gray-600 sm:text-sm"> Showing 1 to 5 of 12 Entries </span>
+      <div class="mt-2 inline-flex sm:mt-0">
+        <button class="mr-2 h-12 w-12 rounded-full border text-sm font-semibold text-gray-600 transition duration-150 hover:bg-gray-100">Prev</button>
+        <button class="h-12 w-12 rounded-full border text-sm font-semibold text-gray-600 transition duration-150 hover:bg-gray-100">Next</button>
+      </div>
+    </div>
   </div>
 </div>
+
 
 </>
     )
