a602a9645c5b24088c9fdea81a6c8d1bf734ff8d
cankirism <cankiri.bilsis@saglik.gov.tr>
2024-09-17 10:07:23 +0300

    google inspection url static setted

--- server/api/index.js
+++ server/api/index.js
@@ -25,7 +25,7 @@ const getOrganicData = async (inspectionUrl) => {
   }
   let indexed = false;
   return unirest
-    .get(seachUrl + inspectionUrl)
+    .get(seachUrl + "https://www.farukgungor.com/")
     .headers({
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36",
