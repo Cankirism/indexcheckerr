c4f3607c2eb2538f5afeeb76d255a200f021d2d3
cankirism <cankiri.bilsis@saglik.gov.tr>
2024-09-30 10:49:09 +0300

    google accept headers random func created

--- server/api/index.js
+++ server/api/index.js
@@ -10,30 +10,44 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "300mb" }));
 const apiLimitter = rate({
-  windowMs: 1000 * 60*24,
+  windowMs: 1000 * 60 * 24,
   max: 1000,
-  message:"Only 10 Request / 1 Day allowed for free plan",
-    
+  message: "Only 10 Request / 1 Day allowed for free plan",
 });
 
-process.on("unhandledRejection",(error)=>{
-  console.log("unhandledrejection",error.message)
-})
+process.on("unhandledRejection", (error) => {
+  console.log("unhandledrejection", error.message);
+});
+const selectRandom = () => {
+  const userAgents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6738.0  Safari/537.36",
+  ];
+  var randomNumber = Math.floor(Math.random() * userAgents.length);
+  return userAgents[randomNumber];
+};
+
+
 const getOrganicData = async (inspectionUrl) => {
   if (!inspectionUrl.endsWith("/")) {
     console.log("/ ile bitmemiş");
     inspectionUrl = inspectionUrl + "/";
   }
   let indexed = false;
+  let user_agent = selectRandom();
+  console.log("user agent is",user_agent);
   return unirest
-    .get(seachUrl+inspectionUrl)
+    .get(seachUrl + inspectionUrl)
     .headers({
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-"User-Agent": " Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
-      
-})
+      "User-Agent": `${user_agent}`,
+    })
     .then(async (response) => {
-      console.log("full url is",seachUrl+inspectionUrl)
+      console.log("full url is", seachUrl + inspectionUrl);
       //console.log("response is",response.body)
       let $ = cheerio.load(response.body);
 
@@ -63,32 +77,30 @@ const getOrganicData = async (inspectionUrl) => {
           displayedLink: displayedLinks[index],
         };
       });
-     
-      
+
       organicResult.map((data) => {
-        console.log("google url",data.links);
+        console.log("google url", data.links);
         console.log(inspectionUrl);
         // if (data.links==inspectionUrl||data.links+"/"==inspectionUrl) {
         //   console.log("google indexxed");
-          
+
         //   indexed = true;
         // }
-        if(data.links.includes(inspectionUrl)){
+        if (data.links.includes(inspectionUrl)) {
           console.log("google indexed");
-          indexed=true;
+          indexed = true;
         }
       });
       return indexed;
     });
-   
 };
 const getBingData = async (inspectionUrl) => {
   let indexed = false;
   if (!inspectionUrl.endsWith("/")) {
     console.log("bind url / ile bitmemiş");
     inspectionUrl = inspectionUrl + "/";
-  };
-  
+  }
+
   return unirest
     .get(bingSearchurl + inspectionUrl)
     .headers({
@@ -101,93 +113,78 @@ const getBingData = async (inspectionUrl) => {
       $(".b_algo .b_tpcn a").each((i, el) => {
         links[i] = $(el).attr("href").split("#")[0];
       });
-     
+
       links.map((link) => {
-        console.log("bing links are",link)
+        console.log("bing links are", link);
         // if (inspectionUrl===link||inspectionUrl===link+"/") {
         //   indexed = true;
         //   console.log("bing indexed")
         // }
-        if(link.includes(inspectionUrl)){
-          indexed=true;
+        if (link.includes(inspectionUrl)) {
+          indexed = true;
           console.log("bing indexed");
         }
       });
       return indexed;
     });
-    
 };
-const getIndexResults = async(url)=>{
-  let googleResult=false;
-  let bingResult=false;
- googleResult = await getOrganicData(url);
+const getIndexResults = async (url) => {
+  let googleResult = false;
+  let bingResult = false;
+  googleResult = await getOrganicData(url);
   bingResult = await getBingData(url);
-  
- 
-  return(
-    {
-      results :{
-        google:googleResult,
-        bind:bingResult
-      }
-    }
 
-  )
-}
-app.post("/api/indexResults",apiLimitter,async(req,res)=>{
-  try{
-   
+  return {
+    results: {
+      google: googleResult,
+      bind: bingResult,
+    },
+  };
+};
+app.post("/api/indexResults", apiLimitter, async (req, res) => {
+  try {
     const inspectionUrls = req.body.url;
     const splittedUrl = inspectionUrls.split(",");
-    if(splittedUrl.length>10){
-    throw new Error("Url count must be < 10")
+    if (splittedUrl.length > 10) {
+      throw new Error("Url count must be < 10");
     }
-  
-   let results = [];
-   await Promise.all(
-    splittedUrl.map(async (item,key)=>{
-     
-      if(isValidUrl(item.trim())){
-        const result = await getIndexResults(item.trim())
-        results.push({
-        url:item,
-        result:result.results
-       })}
-       else {
-        results.push({
-          url:item,
-          result:{google:false,bind:false}
-        })
-       }
- 
-        
+
+    let results = [];
+    await Promise.all(
+      splittedUrl.map(async (item, key) => {
+        if (isValidUrl(item.trim())) {
+          const result = await getIndexResults(item.trim());
+          results.push({
+            url: item,
+            result: result.results,
+          });
+        } else {
+          results.push({
+            url: item,
+            result: { google: false, bind: false },
+          });
+        }
       })
-   )
-   
- console.log("index result is",results);
-    res.status(200).send(results)
-    
+    );
+
+    console.log("index result is", results);
+    res.status(200).send(results);
+  } catch (err) {
+    res.status(400).send(err.message);
   }
-  catch(err){
-    res.status(400).send(
-     err.message
-    )
-  }
-})
+});
 
-const isValidUrl=(url)=>{
- const pattern = new RegExp(
-  '^([a-zA-Z]+:\\/\\/)?' + // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR IP (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$', // fragment locator
-    'i'
+const isValidUrl = (url) => {
+  const pattern = new RegExp(
+    "^([a-zA-Z]+:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR IP (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$", // fragment locator
+    "i"
+  );
+  return pattern.test(url);
+};
 
- );
- return pattern.test(url);
-  
-}
-
-app.listen("5630",()=>console.log("server running on 5630"))
+app.listen("5630", () => console.log("server running on 5630"));
