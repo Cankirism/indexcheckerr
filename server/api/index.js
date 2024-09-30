9dfd836203bf3efbaddfa13bd752071aa31cc59d
cankirism <cankiri.bilsis@saglik.gov.tr>
2024-09-30 10:09:45 +0300

    google headers acept removed

--- server/api/index.js
+++ server/api/index.js
@@ -28,7 +28,7 @@ const getOrganicData = async (inspectionUrl) => {
   return unirest
     .get(seachUrl+inspectionUrl)
     .headers({
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+   //   "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
 "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
     })
     .then(async (response) => {
